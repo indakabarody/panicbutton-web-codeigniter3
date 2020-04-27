b4b2ca722e0a326ca34faccab4bdc9978d954fbd
Indaka Barody <indakabarody16@gmail.com>
2020-04-27 18:23:53 +0700

    Changed admin picture

--- application/views/header.php
+++ application/views/header.php
@@ -76,7 +76,7 @@
 				<!-- Sidebar user (optional) -->
 				<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 					<div class="image">
-						<img src="<?php echo base_url(); ?>assets/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+						<img src="<?php echo base_url(); ?>assets/dist/img/admin.jpg" class="img-circle elevation-2" alt="User Image">
 					</div>
 					<div class="info">
 						<a href="#" class="d-block">Administrator</a>
