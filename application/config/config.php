bb29f5240a848ee73672900d82d32ca3bdd3c2ee
Indaka Barody <indakabarody16@gmail.com>
2020-04-27 18:02:35 +0700

    Switch to dynamic base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/panicbutton3/';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
