c98d14e41c2943a244afaac6abf6505ff18b1e8f
Indaka Barody <indakabarody16@gmail.com>
2020-04-27 18:02:23 +0700

    Fixed bug

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -21,7 +21,7 @@
 	<body class="hold-transition login-page">
 		<div class="login-box">
 			<div class="login-logo">
-				<a href="<?php echo base_url(); ?>assets/index2.html"><b>RS</b> Panic Button</a>
+				<a href="<?php echo base_url(); ?>"><b>RS</b> Panic Button</a>
 			</div>
 			<!-- /.login-logo -->
 			<div class="card">
